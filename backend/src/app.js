ab2c429dff2e3c2b73f95a8c39635b83fec4f05f
Edy Rojas <edyrojas64@gmail.com>
2024-09-16 18:59:35 -0600

    estructura de archivos y conexion con la db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const bodyParser = require("body-parser")
+const cors = require("cors")
 const app = express();
-const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World, is good to see u <3!');
-})
+app.use(bodyParser.json());
+app.use(cors());
+//app.use('/api', userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server run on http://localhost:${port}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
