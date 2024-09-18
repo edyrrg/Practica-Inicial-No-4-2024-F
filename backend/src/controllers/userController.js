dba1c85d7ac7cd539232fd196143641e5d0013ac
Edy Rojas <edyrojas64@gmail.com>
2024-09-18 02:28:17 -0600

    agregando los endpoints y routes necesarias para el login, registro y reinicio de contraseña

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 // controllers/userController.js
+const { parse } = require('dotenv');
 const User = require('../models/User');
 
 exports.getAllUsers = async (req, res) => {
@@ -12,7 +13,31 @@ exports.getAllUsers = async (req, res) => {
 
 exports.getUserById = async (req, res) => {
     try {
-        const user = await User.getById(req.params.id);
+        const id = req.params.id;
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)) {
+            return res.status(400).json({ error: 'Invalid id' });
+        }
+        const user = await User.getById(parsedId);
+        if (user) {
+            return res.status(200).json(user);
+        } else {
+            return res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Error retrieving user' });
+    }
+};
+
+exports.findUserByCarnet = async (req, res) => {
+    try {
+        const { carnet } = req.query;
+        console.log(carnet);
+        if (!carnet) {
+            return res.status(400).json({ error: 'Missing carnet_id' });
+        }
+        const parsedCarnetId = parseInt(carnet);
+        const user = await User.findByCarnet(parsedCarnetId);
         if (user) {
             res.status(200).json(user);
         } else {
@@ -21,7 +46,7 @@ exports.getUserById = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Error retrieving user' });
     }
-};
+}
 
 exports.createUser = async (req, res) => {
     try {
@@ -36,9 +61,21 @@ exports.createUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
     try {
-        await User.update(req.params.id, req.body);
-        res.status(200).json({ message: 'User updated successfully' });
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'Invalid id' });
+        }
+
+        if (!req.body.carnetID || !req.body.nombres || !req.body.apellidos || !req.body.correo) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+
+        const msg = await User.update(id, req.body);
+        res.status(200).json({ ...msg });
     } catch (error) {
+        if (error.message === 'User not found or no changes made') {
+            return res.status(404).json({ error: error.message });
+        }
         res.status(500).json({ error: 'Error updating user' });
     }
 };
@@ -54,13 +91,34 @@ exports.deleteUser = async (req, res) => {
 
 exports.login = async (req, res) => {
     try {
-        const user = await User.getByEmail(req.body.correo);
-        if (user && user.pass === req.body.pass) {
+        if (!req.body.correo || !req.body.pass) {
+            return res.status(400).json({ error: 'Missing email or password' });
+        }
+
+        const user = await User.getCredentialsByEmail(req.body.correo);
+        if (user && user.contraseña === req.body.pass) {
             res.status(200).json({ message: 'Login successful' });
         } else {
             res.status(401).json({ error: 'Invalid credentials' });
         }
     } catch (error) {
         res.status(500).json({ error: 'Error logging in' });
+    }
+}
+
+exports.resetPassword = async (req, res) => {
+    try {
+        if (!req.body.carnet_id || !req.body.new_pass) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        const carnet_id = parseInt(req.body.carnet_id);
+        const msg = await User.resetPassword(carnet_id, req.body.new_pass);
+        
+        res.status(200).json({ ...msg });
+    } catch (error) {
+        if (error.message === 'User not found or pass no changes made') {
+            return res.status(404).json({ error: error.message });
+        }
+        res.status(500).json({ error: 'Error reset password' });
     }
 }
